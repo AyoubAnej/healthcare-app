35c7e0c62a782a71fd39dea4e58a245008e5f95b
AyoubAnej <alyapani9@gmail.com>
2024-08-26 04:59:42 +0100

    add new-appointmet route and create AppointmentForm component

--- app/patients/[userId]/new-appointment/page.tsx
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -1,12 +1,16 @@
-import PatientForm from "@/components/forms/PatientForm";
+import AppointmentForm from "@/components/forms/AppointmentForm";
+import { getPatient } from "@/lib/actions/patient.action";
 import Image from "next/image";
-import Link from "next/link";
 
-export default function NewAppointment() {
+export default async function NewAppointment({
+  params: { userId },
+}: SearchParamProps) {
+  const patient = await getPatient(userId);
+
   return (
     <div className="flex h-screen max-h-screen">
-      <section className="remove-scrollbar container my-auto">
-        <div className="sub-container max-w-[496px]">
+      <section className="remove-scrollbar container">
+        <div className="sub-container max-w-[860px] flex-1 justify-between">
           <Image
             src="/assets/icons/logo-full.svg"
             height={1000}
@@ -14,23 +18,20 @@ export default function NewAppointment() {
             alt="patient"
             className="mb-12 h-10 w-fit"
           />
-          <PatientForm />
-          <div className="text-14-regular mt-20 flex justify-between">
-            <p className="justify-items-end text-dark-600 xl:text-left">
-              © 2024 CarePulse
-            </p>
-            <Link href="/?admin=true" className="text-green-500">
-              Admin
-            </Link>
-          </div>
+          <AppointmentForm
+            type="create"
+            userId={userId}
+            patientId={patient.$id}
+          />
+          <p className="copyright mt-10 py-12">© 2024 CarePulse</p>
         </div>
       </section>
       <Image
-        src="/assets/images/onboarding-img.png"
+        src="/assets/images/appointment-img.png"
         height={1000}
         width={1000}
-        alt="patient"
-        className="side-img max-w-[50%]"
+        alt="appointment"
+        className="side-img max-w-[390px] bg-bottom"
       />
     </div>
   );
